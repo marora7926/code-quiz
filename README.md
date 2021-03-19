fae05b76e46e37ac043cb3c73090caad17d3851e
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-20 02:54:48 +1100

    changes to readme, index and css file

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# code-quiz
+# 04 Web APIs: Code Quiz
+
+## Your Task
+
+This piece of work enhance my skills to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that are included. It has a clean, polished, and responsive user interface.
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+## Mock-Up
+
+The following sets of images demonstrates the overview of the web page:
+
+(./Assets/04-web-apis-homework-demo.gif)
+
+---
+
+© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+// quiz questions
+// What tag can be used to insert a line break or blank line in an HTML document?
+// <title></title>
+// <br></br> - A
+// <head></head>
+// <body></body>
+
+// What group of tags are used to define the text headers in the body of the HTML document?
+// "<footer>"
+// "<h1> to <h3>" - A
+// <td>
+// <button>
+
+// The correct sequence of HTML tags for starting a webpage is -
+// Head, Title, HTML, body
+// HTML, Body, Title, Head
+// HTML, Title, Head, Body
+// HTML, Head, Title, Body
+
+// What is the most important CSS property, used for controlling the layout?
+// Display - A
+// Margin
+// Padding
+// <div>
+
+// What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?
+// Scope - A
+// Range
+// Restriction
+// Output Level  
+
+// Which of the following is the correct syntax to select the p siblings of a div element?
+// p
+// div + p
+// div p
+// div ~ p - A
+
+// Where is the JavaScript placed inside an HTML document or page?
+// In the <title> section.
+// In the <meta> section.
+// In the <footer> section.
+// In the <body> and <head> sections. - A
+
+// https://www.freecodecamp.org/news/multiple-choice-quiz-template/
