a38b0344fc1ad6882fd9ebc6fcb3fc225aaebb35
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-20 15:49:29 +1100

    quiz questions js

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
+//  defining all the variables
+var
 
-
-// quiz questions
-// What tag can be used to insert a line break or blank line in an HTML document?
-// <title></title>
-// <br></br> - A
-// <head></head>
-// <body></body>
-
-// What group of tags are used to define the text headers in the body of the HTML document?
-// "<footer>"
-// "<h1> to <h3>" - A
-// <td>
-// <button>
-
-// The correct sequence of HTML tags for starting a webpage is -
-// Head, Title, HTML, body
-// HTML, Body, Title, Head
-// HTML, Title, Head, Body
-// HTML, Head, Title, Body
-
-// What is the most important CSS property, used for controlling the layout?
-// Display - A
-// Margin
-// Padding
-// <div>
-
-// What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?
-// Scope - A
-// Range
-// Restriction
-// Output Level  
-
-// Which of the following is the correct syntax to select the p siblings of a div element?
-// p
-// div + p
-// div p
-// div ~ p - A
-
-// Where is the JavaScript placed inside an HTML document or page?
-// In the <title> section.
-// In the <meta> section.
-// In the <footer> section.
-// In the <body> and <head> sections. - A
+// Question bank for quiz as "arrays"
+var questions =[{
+    question: "What tag can be used to insert a line break or blank line in an HTML document?",
+    options: [
+        "<title></title>",
+        "<br></br>",
+        "<head></head>",
+        "<body></body>",
+    ],
+    answer: "<br></br>" 
+    },{
+    question: "What group of tags are used to define the text headers in the body of the HTML document?",
+    options: [
+        "<h1> to <h3>",
+        "<td>",
+        "<footer>",
+        "<button>",
+        ],
+    answer: "<h1> to <h3>"   
+    },{
+    question: "The correct sequence of HTML tags for starting a webpage is -",
+    options: [
+        "Head, Title, HTML, body",
+        "HTML, Body, Title, Head",
+        "HTML, Title, Head, Body",
+        "HTML, Head, Title, Body",
+        ],
+    answer: "HTML, Head, Title, Body"  
+    },{
+    question: "What is the most important CSS property, used for controlling the layout?",
+    options: [
+        "Display",
+        "Margin",
+        "Padding",
+        "<div>",
+        ],
+    answer: "Display"  
+    },{
+    question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+    options: [
+        "Scope",
+        "Range",
+        "Restriction",
+        "Output Level",
+        ],
+    answer: "Scope"  
+    },{
+    question: "Which of the following is the correct syntax to select the p siblings of a div element?",
+    options: [
+        "p",
+        "div + p",
+        "div p",
+        "div ~ p",
+        ],
+    answer: "div ~ p"  
+    },{
+    question: "Where is the JavaScript placed inside an HTML document or page?",
+    options: [
+        "In the <title> section",
+        "In the <meta> section",
+        "In the <footer> section",
+        "In the <body> and <head> sections",
+        ],
+        answer: "In the <body> and <head> sections"  
+    }
+]
 
 // https://www.freecodecamp.org/news/multiple-choice-quiz-template/
