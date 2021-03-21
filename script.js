2c11e1ab3ff1b421845b88ed6a8515765c5ef59b
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-21 19:12:28 +1100

    progressing questiona page

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+// VARIABLES to set the scene
 //  defining all query selector
 var liveTimeValue = document.querySelector(".live-time-value");
-var dispalyQuestion = document.querySelector(".question");
-var answer = document.querySelector(".answer");
+var displayScreen = document.querySelector(".screen");
+var correctOrWrong = document.querySelector(".answer");
 var highScores = document.querySelector(".High-scores");
-
 
 // Question bank for quiz as "arrays"
 var questions =[{
@@ -72,17 +72,22 @@ var questions =[{
     }
 ]
 
-//setting the screen that users will see when they launch the quiz 
+//variable for setting the screen that users will see when they launch the quiz 
 var startScreen = {
     startTitle: "Coding Quiz Challenge",
-    startMessage: "Instructions: Try to answer the following code related questions within the time limit. Keep in mind that every incorrect answers will penalize your scoretime by 10 seconds. Press START key to start the quiz. Good luck!",
+    startInstructions: "Instructions: Try to answer the following code related questions within the time limit. Keep in mind that every incorrect answers will penalize your scoretime by 10 seconds. Press START key to start the quiz. Good luck!",
     startKey: "Start"
 }
 
-// variables for displaying the users if their answer is correct or wrong
+// variable for displaying the users if their answer is correct or wrong
 var answerDisplay = {
     correctAnswer: "Correct!",
     wrongAnswer: "Wrong!",
+}
+
+// variable for displaying "out of time" when user do not compelte all question in allocated time
+var timeOver = {
+    timeOverheading: "The quiz is terminated because you run out of time"
 }
 
 // variables for displaying the user for input additional information when the quiz is complete
@@ -93,13 +98,13 @@ var quizComplete = {
     completionButton: "Submit to archive"
 }
 
-// variables for saving users details as entered above
+// variable for saving users details as entered above
 var savedDetails = {
     userScore: 0,
     userInitials: ""
 }
 
-// varaible to display saved user-entered detials and their scores to the highscores buttton
+// variable to display saved user-entered detials and their scores to the highscores buttton
 var highScores = {
     highScoresheader: "High Scores",
     highScoresInitials: "Initials",
@@ -108,4 +113,143 @@ var highScores = {
     highScoresClearButton: "Clear High Scores"
 }
 
-// https://www.freecodecamp.org/news/multiple-choice-quiz-template/
+// FUNCTIONS to make the website interactive
+
+    // ENTRY SCREEN - function and variables what user will see on the "entry screen"
+        // Entry screen title
+    function entryScreen () {
+        // entry screen title 
+        var startScreenTitle = document.createElement("h1");
+            startScreenTitle.textContent = startScreen.startTitle;
+            startScreenTitle.setAttribute("class", "startTitle");
+            displayScreen.appendChild(startScreenTitle);
+
+        // entry screen instructions
+        var startScreenInstructions = document.createElement("p");
+            startScreenInstructions.textContent = startScreen.startInstructions;
+            startScreenInstructions.setAttribute("class", "startInstructions");
+            displayScreen.appendChild(startScreenInstructions);
+
+        // entry screen start button
+        var startScreenButton = document.createElement("button");
+            startScreenButton.textContent = startScreen.startkey;
+            startScreenButton.setAttribute("class", "startkey");
+            displayScreen.appendChild(startScreenButton);
+
+        // defining the time on the live timer on the entry screen
+        var totalSeconds = 50
+        liveTimeValue.textContent = totalSeconds;
+
+        // eventListner to start the even, in this case, start the quiz on the entry screen
+        startScreenButton.addEventListener("click", startCodeQuiz);
+    }
+
+
+    // TRANSITION ENTRY TO QUESTION SCREEN - clear the content on the entry screen for the next screen i.e., questions for the quiz
+    function clearScreen() {
+        displayScreen.innerHTML = "";
+    } 
+
+    // QUESTION SCREEN - function and variables what user will see (on the "question screen") after pressing the start key on the "entry screen"
+    function startCodeQuiz() {
+        // live timer to start at the launch of the quiz on the "question screen"
+        var interval;
+        var secondsLeft = 0;
+
+        function startLiveTimer(){
+            interval = setInterval
+            (function() {
+                secondsLeft++;
+                updateTimerDisplay();
+            if (secondsLeft > totalSeconds) {
+                finishTimer();
+                }
+            }, 1000);
+        }
+        
+        // setting question numner at the start of the quiz on the "question screen"  
+        var questionNumber = 0;
+        // style of the question to be displayed on the "question screen"
+        function questionStyle(){
+            // display question statement and its format on the "question screen"
+            var questionStatement = document.createElement("p");
+            questionStatement.textContent = questions[questionNumber].question;
+            questionStatement.setAttribute("class", "question");
+            displayScreen.appendChild(questionStatement);
+
+            // display answer choice (unordered list element) on the "question screen" for each question
+            var questionUl = document.createElement("ul");
+            questionUl.setAttribute("class", "questionUl");
+            displayScreen.appendChild(questionUl);
+            
+            //question list element
+            for (var i = 0; i < questions[questionNumber].option.length; i++){
+            var questionLi = document.createElement("li");
+            questionLi.setAttribute("class", "questionLi");
+            questionUl.appendChild(questionLi);
+
+            // allocating button key to answers options
+            var answerOptionsButton = document.createElement("button");
+            answerOptionsButton.textContent = questions[questionNumber].option[i];
+            answerOptionsButton.setAttribute("class", "answerOptionsButton");
+            questionLi.appendChild(answerOptionsButton);
+
+            //checking the answer on the "question screen"
+            answerOptionsButton.addEventListener("click", validateAnswer);
+            }
+        }
+        
+        // validating (correct or wrong) the user input on the "question screen"
+        // variable for displaying the correct or wrong message 
+        var correctWrongMessage = {
+            correctMessage = "Congratulations! Your answer in CORRECT",
+            wrongMessage = "Your answer in Wrong",
+            displayTime: 1500,
+        }
+
+        // score variable for baseline score
+        var score = 0;
+        var penaltyWrongAnswer = 10;
+        
+        function validateAnswer(event) {
+                // when an answer option is clicked - two components, as below:
+            // a) for correct answer 
+            if (event.target.textContent == questions[questionNumber].answer){
+            // b) for wrong answer 
+                correctOrWrong.textContent = correctWrongMessage.correctMessage
+                score++;
+            } else {
+                correctOrWrong.textContent = correctWrongMessage.wrongMessage
+                secondsLeft += penaltyWrongAnswer;
+            }
+
+        
+        }
+        
+        // display each question on one "question screen"
+        function clearScreen();
+        if (questionNumber === questions.length - 1) {
+            // will be defined subsequently
+            stopTimer();
+            // display allDone screen (this will be defined subsequently)
+            displayAllDone();
+        } else {
+            // move to next question
+            questionNumber++;
+            // displays the next question on the "question screen"
+            questionStyle();
+        }
+    }
+
+    
+
+
+
+
+    }
+       
+    
+    
+
+
+// Reference website: https://www.freecodecamp.org/news/multiple-choice-quiz-template/
